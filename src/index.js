84696d590492b13c17141749bda5b99502f5ad01
zackdk <958059970@qq.com>
2019-11-29 15:30:54 +0800

    样式修改

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './reset.css';
 import 'antd-mobile/dist/antd-mobile.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
