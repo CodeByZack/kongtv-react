33270870b49a636cb36c132fb3529fd51b10932d
zackdk <958059970@qq.com>
2020-01-19 17:43:37 +0800

    使用自己写的reduxtool

--- src/store/index.js
+++ src/store/index.js
@@ -4,21 +4,28 @@ import detail from './modules/detail';
 import search from './modules/search';
 import createRematchPersist from '@rematch/persist';
 import { init } from '@rematch/core';
+import ReduxTool from "../utils/reduxtool";
+import { createStore } from "redux";
 
-const persistPlugin = createRematchPersist({
-  whitelist: ['play', 'detail', 'search'],
-  throttle: 1000,
-  version: 1,
-});
+// const persistPlugin = createRematchPersist({
+//   whitelist: ['play', 'detail', 'search'],
+//   throttle: 1000,
+//   version: 1,
+// });
 
-const store = init({
-  models: {
-    home,
-    play,
-    detail,
-    search,
-  },
-  plugins: [persistPlugin],
-});
+// const store = init({
+//   models: {
+//     home,
+//     play,
+//     detail,
+//     search,
+//   },
+//   plugins: [persistPlugin],
+// });
+
+const store = createStore(ReduxTool.createReducer({
+  home,play,detail,search
+}));
+ReduxTool.ejectDispatch(store);
 
 export default store;
---
+++ src/utils/reduxtool.js
@@ -0,0 +1,85 @@
+const createReducer = models => {
+  const finalReducer = {};
+  const defaultState = {};
+  const nameSpaces = Object.keys(models);
+  for (let index = 0; index < nameSpaces.length; index++) {
+    const nameSpace = nameSpaces[index];
+    const model = models[nameSpace];
+    //处理state
+    defaultState[nameSpace] = model.state;
+    //处理reducer
+    const modelReducerKeys = Object.keys(model.reducers);
+    for (let index = 0; index < modelReducerKeys.length; index++) {
+      const reudcerKey = modelReducerKeys[index];
+      finalReducer[`${nameSpace}/${reudcerKey}`] = model.reducers[reudcerKey];
+    }
+  }
+  return (state = defaultState, action) => {
+    const reducer = finalReducer[action.type];
+    const newState = { ...state };
+    if (reducer) {
+      const { type, payload } = action;
+      const [modelName] = type.split('/');
+      newState[modelName] = reducer(state[modelName], ...payload);
+      return newState;
+    } else {
+      console.log('未找到注册的Reducers');
+      return newState;
+    }
+  };
+};
+
+const ejectDispatch = (store, models) => {
+  const { dispatch, getState } = store;
+  const nameSpaces = Object.keys(models);
+  for (let index = 0; index < nameSpaces.length; index++) {
+    const nameSpace = nameSpaces[index];
+    const model = models[nameSpace];
+    //把所有方法注入dispatch
+    //为所有effect绑定this
+    model.effects = model.effects ? model.effects : {};
+    dispatch[nameSpace] = {};
+    const modelEffectKeys = Object.keys(model.effects);
+    for (let index = 0; index < modelEffectKeys.length; index++) {
+      const effectKey = modelEffectKeys[index];
+      dispatch[nameSpace][effectKey] = (args)=>{
+        model.effects[effectKey].call(
+          dispatch[nameSpace],
+          args,
+          getState()
+        );
+      } 
+    }
+    //替换所有reducer方法
+    const modelReducerKeys = Object.keys(model.reducers);
+    for (let index = 0; index < modelReducerKeys.length; index++) {
+      const reudcerKey = modelReducerKeys[index];
+      const type = `${nameSpace}/${reudcerKey}`;
+      dispatch[nameSpace][reudcerKey] = (...payload) =>
+        dispatch({ type, payload });
+    }
+  }
+  return dispatch;
+};
+
+let _models = null;
+
+const reduxtool = {
+  createReducer: models => {
+    _models = models;
+    return createReducer(_models);
+  },
+  ejectDispatch: dispatch => {
+    if (!_models) {
+      console.log('请先调用ReduxTool.createReducer()注入models');
+      return;
+    }
+    if (!dispatch) {
+      console.log('请传入redux/store的dispatch方法');
+      return;
+    }
+    ejectDispatch(dispatch, _models);
+  },
+};
+
+export default reduxtool;
