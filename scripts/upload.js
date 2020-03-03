7c0ee5b03c33fc198f2ff115f9bfe1fa7a256fa0
董凯 <dongk@travelsky.com>
2020-03-03 15:27:57 +0800

    上传到cos的脚本

---
+++ scripts/upload.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const uploadQcloud = require('qcloud-upload');
+const argv = require('yargs').argv;
+
+if (!argv.SecretId || !argv.SecretKey) {
+  throw (new Error("输入SecretId和SecretKey"));
+}
+
+const options = {
+  Region: 'ap-chengdu',
+  SecretId: argv.SecretId,
+  SecretKey: argv.SecretKey,
+  Bucket: 'apks-1252514056',
+  Headers: {
+    CacheControl: 'max-age=31536000'
+  },
+  prefix: 'web',
+  src: '../build',
+  overWrite: true
+};
+uploadQcloud(options);
