d7a697c220f6a5a16a6911d2e7a81bac1c9f7104
董凯 <dongk@travelsky.com>
2020-03-27 17:29:37 +0800

    更新 react引入

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -722,5 +722,9 @@ module.exports = function(webpackEnv) {
     // Turn off performance processing because we utilize
     // our own hints via the FileSizeReporter
     performance: false,
+    externals : {
+      'react': 'React',
+      'react-dom': 'ReactDOM'
+    }
   };
 };
