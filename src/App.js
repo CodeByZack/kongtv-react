ea8ed0a7ef4b55feaa2227e1a9e2a68190d70771
zackdk <958059970@qq.com>
2020-10-27 18:05:01 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MovieSearch from '@/page/search';
 import { Fade, Slide } from '@material-ui/core';
 import ScrollToTop from '@/components/scrollToTop';
 import { BrowserRouter, Route, Switch, useLocation } from 'react-router-dom';
+import { checkBrowser } from './utils';
 
 const routes = [
   { path: '/play', Component: PlayMovie },
@@ -17,17 +18,37 @@ const routes = [
   { path: '/', Component: Home },
 ];
 
+
+const isSafari = checkBrowser() === 'Safari';
+
 const renderWithTransition = (path, Component) => {
   const render = props => {
     const { match, history } = props;
-    const direction = history.action === 'POP' ? 'right' : 'left';
-    return (
-      <Slide direction={direction} in={match != null} unmountOnExit>
-        <div className="page">
-          <Component />
-        </div>
-      </Slide>
-    );
+    console.log("isSafari",isSafari);
+    
+    
+    if(!isSafari){
+      const direction = history.action === 'POP' ? 'right' : 'left';
+      return (
+        <Slide direction={direction} in={match != null} unmountOnExit>
+          <div className="page">
+            <Component />
+          </div>
+        </Slide>
+      );
+    }else{
+      if(history.action === 'POP'){
+        return <Component />
+      }else{
+        return (
+          <Slide direction='left' in={match != null} unmountOnExit>
+            <div className="page">
+              <Component />
+            </div>
+          </Slide>
+        );
+      }
+    }
   };
   return (
     <Route key={path} exact path={path}>
--- src/utils/index.js
+++ src/utils/index.js
@@ -21,3 +21,22 @@ export const getQuery = str => {
   }, {});
   return obj;
 };
+
+export const checkBrowser = ()=>{
+  const userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+  if (userAgent.indexOf("Opera") > -1) {
+      return "Opera"
+  }; //判断是否Opera浏览器
+  if (userAgent.indexOf("Firefox") > -1) {
+      return "FF";
+  } //判断是否Firefox浏览器
+  if (userAgent.indexOf("Chrome") > -1){
+    return "Chrome";
+  }
+  if (userAgent.indexOf("Safari") > -1) {
+      return "Safari";
+  } //判断是否Safari浏览器
+  // if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+  //     return "IE";
+  // }; //判断是否IE浏览器
+}
