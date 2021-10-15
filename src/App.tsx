e1539102cc442322819101a18bfb4eb108f5f5a2
dongkai <958059970@qq.com>
2021-10-15 11:32:53 +0800

    初始化vite+ts+react项目

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-// import Icon from './icon';
-import RatioImage from './ratioImage';
-import Toast from './toast';
-import SearchBar from './searchBar';
-import Loading from './loading';
-import MyAppBar from './myAppBar';
-
-export { RatioImage, Toast, SearchBar, Loading, MyAppBar };
