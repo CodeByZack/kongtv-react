885af162ca2f88a23c8509d20aa1cbd550a9a7c2
Air <airsmith@AirdeMacBook-Air.local>
2021-10-19 17:32:47 +0800

    swiper init

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { RouteComponentProps } from "@reach/router";
+import HomeMain from './HomeMain'
+import store from '../../store';
 interface IProps extends RouteComponentProps {};
 
 
 const Home = (props : IProps)=>{
+    const { home } = store.useContainer();
+    const {  adviceMovieList } = home;
     return (
-        <div>Home</div>
+        <HomeMain data={adviceMovieList}/>
     );
 }
 export default Home;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+/* 
+    * ts图片声明导入文件
+*/
+// declare module '*.scss' {
+//     const css: { [key: string]: string };
+//     export default css;
+//   }
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
