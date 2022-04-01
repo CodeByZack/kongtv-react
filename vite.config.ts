ec8bfa47f3818d2aab7e4f2ccabf248d6d929920
dongkai <958059970@qq.com>
2022-04-01 11:31:26 +0800

    修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    outDir: 'build',
     rollupOptions: {
       // external: ['react', 'react-dom'],
       // output: {
